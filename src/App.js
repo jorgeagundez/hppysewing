3b091260e7b734d403349d62a318dfab28d06e5c
Jorge <jagundez@runroom.com>
2021-02-14 17:00:42 +0100

    Add normalize and first styling

--- src/App.js
+++ src/App.js
@@ -4,25 +4,27 @@ import './App.css';
 function App() {
   return (
     <Fragment>
-      <header className="header">
-        <img src={process.env.PUBLIC_URL + '/img/logo.png'} className="logo" alt="logo" />
-        <p>Bienvenidos a Hppysewing</p>
-      </header>
-      <aside>
-        <p>Carrito</p>
-      </aside>
-      <section>
-        <h1>Sección principal</h1>
-        <div className="productos">
-          <div>Producto 1</div>
-          <div>Producto 2</div>
-        </div>
-      </section>
-      <footer>
-        <nav>
-          Redes sociales
-        </nav>
-      </footer>
+      <div className="contenedor">
+        <header className="header">
+          <img src={process.env.PUBLIC_URL + '/img/logo.png'} className="logo" alt="logo" />
+          <p>Bienvenidos a Hppysewing</p>
+        </header>
+        <aside>
+          <p>Carrito</p>
+        </aside>
+        <section>
+          <h1>Sección principal</h1>
+          <div className="productos">
+            <div>Producto 1</div>
+            <div>Producto 2</div>
+          </div>
+        </section>
+        <footer>
+          <nav>
+            Redes sociales
+          </nav>
+        </footer>
+      </div>
     </Fragment>
   );
 }
