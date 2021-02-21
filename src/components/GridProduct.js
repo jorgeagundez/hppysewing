2f763499a646337a457d55092110bf5b42bda89d
Jorge <jagundez@runroom.com>
2021-02-21 13:53:35 +0100

    Add cta and padding to grid

--- src/components/GridProduct.js
+++ src/components/GridProduct.js
@@ -45,6 +45,7 @@ const GridProduct = ({product, cart, setCart}) => {
             <p>{description}</p>
             <button
                 onClick={() => addToCart()}
+                className="cta-pp"
             >Añadir al carrito</button>
         </div>
     )
